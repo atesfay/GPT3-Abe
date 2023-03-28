715f5949483f8e69a60a1f2b07901fbf623ec89d
Abraham Tesfay <abraham@Abrahams-Mac-mini.local>
2023-03-27 23:45:47 -0700

    Add Navbar component, modifay App.js and index.js

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { RiMenu3line, RiCloseLin } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__nabvar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
